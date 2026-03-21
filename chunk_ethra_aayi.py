8ff2c19bcd1ca772d8dff09730b3a1bab1291337
Erenjaegaaa <saahilmanojmp@ug.cusat.ac.in>
2026-03-21 13:07:13 +0530

    terminal question answering main.py

--- chunk_ethra_aayi.py
+++
@@ -1,9 +0,0 @@
-import json
-with open("./data/chunks/chunks.json") as f:
-    chunks = json.load(f)
-if isinstance(chunks, dict):
-    chunks = list(chunks.values())
-for i, c in enumerate(chunks):
-    if c["chunk_id"] == "c3_89owyn_003":
-        print(i)
-        break
